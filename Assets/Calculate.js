e0f6bd172b01afc5b40b11a3bc6182c32a7c0774
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-10 22:31:43 -0500

    Added App
    This is the first version of Existence Counter that GitHub has ever seen!

---
+++ Assets/Calculate.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+var Year : UnityEngine.UI.Text;
+var Month : UnityEngine.UI.Text;
+var Day : UnityEngine.UI.Text;
+var Hours : UnityEngine.UI.Text;
+var Minutes : UnityEngine.UI.Text;
+var Seconds : UnityEngine.UI.Text;
+
+var Year2 = parseFloat(System.DateTime.UtcNow.ToString("yyyy"));
+var Month2 = parseFloat(System.DateTime.UtcNow.ToString("MM"));
+var Day2 = parseFloat(System.DateTime.UtcNow.ToString("dd"));
+var Hours2 = parseFloat(System.DateTime.UtcNow.ToString("HH"));
+var Minutes2 = parseFloat(System.DateTime.UtcNow.ToString("mm"));
+var Seconds2 = parseFloat(System.DateTime.UtcNow.ToString("ss"));
+
+function Start () {
+	
+	while (1 > 0) {
+		yield WaitForSeconds (1);
+		Application.LoadLevel ("Stats");
+	}
+}
+
+function Update () {
+	Debug.Log(System.DateTime.UtcNow.ToString("ss"));
+	var Seconds3 = ((((PlayerPrefs.GetFloat("Year") - Year2) * 365) + ((PlayerPrefs.GetFloat("Month") - Month2) * 30) + (PlayerPrefs.GetFloat("Day") - Day2)) * -86400 + (Hours2 * 120) + (Minutes2 * 60) + Seconds2);
+	
+	Year.text = Mathf.Abs(PlayerPrefs.GetFloat("Year") - Year2 + ((PlayerPrefs.GetFloat("Month") - Month2) / 12) + ((PlayerPrefs.GetFloat("Day") - Day2) / 365)) + " Years";
+	
+	Month.text = Mathf.Abs(((PlayerPrefs.GetFloat("Year") - Year2) * 12) + (PlayerPrefs.GetFloat("Month") - Month2) + ((PlayerPrefs.GetFloat("Day") - Day2) / 30)) + " Months";
+	
+	Day.text = Mathf.Abs(((PlayerPrefs.GetFloat("Year") - Year2) * 365) + ((PlayerPrefs.GetFloat("Month") - Month2) * 30) + (PlayerPrefs.GetFloat("Day") - Day2)) + " Days";
+	
+	Hours.text = Mathf.Abs((((PlayerPrefs.GetFloat("Year") - Year2) * 365) + ((PlayerPrefs.GetFloat("Month") - Month2) * 30) + (PlayerPrefs.GetFloat("Day") - Day2)) * 24 + Hours2) + " Hours";
+	
+	Minutes.text = Mathf.Abs((((PlayerPrefs.GetFloat("Year") - Year2) * 365) + ((PlayerPrefs.GetFloat("Month") - Month2) * 30) + (PlayerPrefs.GetFloat("Day") - Day2)) * 1440 + (Hours2 * 60) + Minutes2) + " Minutes";
+	
+	Seconds.text = Seconds3.ToString("n0") + " Seconds";
+}
+
+function Reset () {
+	PlayerPrefs.DeleteAll();
+	Application.LoadLevel ("MainMenu");
+}
+
---
+++ Assets/Number.js
@@ -0,0 +1,264 @@
+﻿#pragma strict
+var Month : UnityEngine.UI.Dropdown;
+var Day : UnityEngine.UI.Dropdown;
+var Year : UnityEngine.UI.Dropdown;
+	
+function Start () {
+	Debug.Log(PlayerPrefs.GetFloat("Year"));
+	if (PlayerPrefs.GetFloat("Month") != 0 && PlayerPrefs.GetFloat("Day") != 0 && PlayerPrefs.GetFloat("Year") != 0) {
+		Application.LoadLevel ("Stats");
+	}
+}
+
+function Change () {
+	if (PlayerPrefs.GetFloat("Month") != 0 && PlayerPrefs.GetFloat("Day") != 0 && PlayerPrefs.GetFloat("Year") != 0) {
+		Application.LoadLevel ("Stats");
+	}
+}
+
+
+function Months () {
+	PlayerPrefs.SetFloat("Month", Month.value);
+}
+
+function Days () {
+	PlayerPrefs.SetFloat("Day", Day.value);
+}
+
+function Years () {
+	if (Year.value == 1) {                            //Jesus Christ this is long
+		PlayerPrefs.SetFloat("Year", 2016);
+	}else if (Year.value == 2) {
+		PlayerPrefs.SetFloat("Year", 2015);
+	}else if (Year.value == 3) {
+		PlayerPrefs.SetFloat("Year", 2014);
+	}else if (Year.value == 4) {
+		PlayerPrefs.SetFloat("Year", 2013);
+	}else if (Year.value == 5) {
+		PlayerPrefs.SetFloat("Year", 2012);
+	}else if (Year.value == 6) {
+		PlayerPrefs.SetFloat("Year", 2011);
+	}else if (Year.value == 7) {
+		PlayerPrefs.SetFloat("Year", 2010);
+	}else if (Year.value == 8) {
+		PlayerPrefs.SetFloat("Year", 2009);
+	}else if (Year.value == 9) {
+		PlayerPrefs.SetFloat("Year", 2008);
+	}else if (Year.value == 10) {
+		PlayerPrefs.SetFloat("Year", 2007);
+	}else if (Year.value == 11) {
+		PlayerPrefs.SetFloat("Year", 2006);
+	}else if (Year.value == 12) {
+		PlayerPrefs.SetFloat("Year", 2005);
+	}else if (Year.value == 13) {
+		PlayerPrefs.SetFloat("Year", 2004);
+	}else if (Year.value == 14) {
+		PlayerPrefs.SetFloat("Year", 2003);
+	}else if (Year.value == 15) {
+		PlayerPrefs.SetFloat("Year", 2002);
+	}else if (Year.value == 16) {
+		PlayerPrefs.SetFloat("Year", 2001);
+	}else if (Year.value == 17) {
+		PlayerPrefs.SetFloat("Year", 2000);
+	}else if (Year.value == 18) {
+		PlayerPrefs.SetFloat("Year", 1999);
+	}else if (Year.value == 19) {
+		PlayerPrefs.SetFloat("Year", 1998);
+	}else if (Year.value == 20) {
+		PlayerPrefs.SetFloat("Year", 1997);
+	}else if (Year.value == 21) {
+		PlayerPrefs.SetFloat("Year", 1996);
+	}else if (Year.value == 22) {
+		PlayerPrefs.SetFloat("Year", 1995);
+	}else if (Year.value == 23) {
+		PlayerPrefs.SetFloat("Year", 1994);
+	}else if (Year.value == 24) {
+		PlayerPrefs.SetFloat("Year", 1993);
+	}else if (Year.value == 25) {
+		PlayerPrefs.SetFloat("Year", 1992);
+	}else if (Year.value == 26) {
+		PlayerPrefs.SetFloat("Year", 1991);
+	}else if (Year.value == 27) {
+		PlayerPrefs.SetFloat("Year", 1990);
+	}else if (Year.value == 28) {
+		PlayerPrefs.SetFloat("Year", 1989);
+	}else if (Year.value == 29) {
+		PlayerPrefs.SetFloat("Year", 1988);
+	}else if (Year.value == 30) {
+		PlayerPrefs.SetFloat("Year", 1987);
+	}else if (Year.value == 31) {
+		PlayerPrefs.SetFloat("Year", 1986);
+	}else if (Year.value == 32) {
+		PlayerPrefs.SetFloat("Year", 1985);
+	}else if (Year.value == 33) {
+		PlayerPrefs.SetFloat("Year", 1984);
+	}else if (Year.value == 34) {
+		PlayerPrefs.SetFloat("Year", 1983);
+	}else if (Year.value == 35) {
+		PlayerPrefs.SetFloat("Year", 1982);
+	}else if (Year.value == 36) {
+		PlayerPrefs.SetFloat("Year", 1981);
+	}else if (Year.value == 37) {
+		PlayerPrefs.SetFloat("Year", 1980);
+	}else if (Year.value == 38) {
+		PlayerPrefs.SetFloat("Year", 1979);
+	}else if (Year.value == 39) {
+		PlayerPrefs.SetFloat("Year", 1978);
+	}else if (Year.value == 40) {
+		PlayerPrefs.SetFloat("Year", 1977);
+	}else if (Year.value == 41) {
+		PlayerPrefs.SetFloat("Year", 1976);
+	}else if (Year.value == 42) {
+		PlayerPrefs.SetFloat("Year", 1975);
+	}else if (Year.value == 43) {
+		PlayerPrefs.SetFloat("Year", 1974);
+	}else if (Year.value == 44) {
+		PlayerPrefs.SetFloat("Year", 1973);
+	}else if (Year.value == 45) {
+		PlayerPrefs.SetFloat("Year", 1972);
+	}else if (Year.value == 46) {
+		PlayerPrefs.SetFloat("Year", 1971);
+	}else if (Year.value == 47) {
+		PlayerPrefs.SetFloat("Year", 1970);
+	}else if (Year.value == 48) {
+		PlayerPrefs.SetFloat("Year", 1969);
+	}else if (Year.value == 49) {
+		PlayerPrefs.SetFloat("Year", 1968);
+	}else if (Year.value == 50) {
+		PlayerPrefs.SetFloat("Year", 1967);
+	}else if (Year.value == 51) {
+		PlayerPrefs.SetFloat("Year", 1966);
+	}else if (Year.value == 52) {
+		PlayerPrefs.SetFloat("Year", 1965);
+	}else if (Year.value == 53) {
+		PlayerPrefs.SetFloat("Year", 1964);
+	}else if (Year.value == 54) {
+		PlayerPrefs.SetFloat("Year", 1963);
+	}else if (Year.value == 55) {
+		PlayerPrefs.SetFloat("Year", 1962);
+	}else if (Year.value == 56) {
+		PlayerPrefs.SetFloat("Year", 1961);
+	}else if (Year.value == 57) {
+		PlayerPrefs.SetFloat("Year", 1960);
+	}else if (Year.value == 58) {
+		PlayerPrefs.SetFloat("Year", 1959);
+	}else if (Year.value == 59) {
+		PlayerPrefs.SetFloat("Year", 1958);
+	}else if (Year.value == 60) {
+		PlayerPrefs.SetFloat("Year", 1957);
+	}else if (Year.value == 61) {
+		PlayerPrefs.SetFloat("Year", 1956);
+	}else if (Year.value == 62) {
+		PlayerPrefs.SetFloat("Year", 1955);
+	}else if (Year.value == 63) {
+		PlayerPrefs.SetFloat("Year", 1954);
+	}else if (Year.value == 64) {
+		PlayerPrefs.SetFloat("Year", 1953);
+	}else if (Year.value == 65) {
+		PlayerPrefs.SetFloat("Year", 1952);
+	}else if (Year.value == 66) {
+		PlayerPrefs.SetFloat("Year", 1951);
+	}else if (Year.value == 67) {
+		PlayerPrefs.SetFloat("Year", 1950);
+	}else if (Year.value == 68) {
+		PlayerPrefs.SetFloat("Year", 1949);
+	}else if (Year.value == 69) {
+		PlayerPrefs.SetFloat("Year", 1948);
+	}else if (Year.value == 70) {
+		PlayerPrefs.SetFloat("Year", 1947);
+	}else if (Year.value == 71) {
+		PlayerPrefs.SetFloat("Year", 1946);
+	}else if (Year.value == 72) {
+		PlayerPrefs.SetFloat("Year", 1945);
+	}else if (Year.value == 73) {
+		PlayerPrefs.SetFloat("Year", 1944);
+	}else if (Year.value == 74) {
+		PlayerPrefs.SetFloat("Year", 1943);
+	}else if (Year.value == 75) {
+		PlayerPrefs.SetFloat("Year", 1942);
+	}else if (Year.value == 76) {
+		PlayerPrefs.SetFloat("Year", 1941);
+	}else if (Year.value == 77) {
+		PlayerPrefs.SetFloat("Year", 1940);
+	}else if (Year.value == 78) {
+		PlayerPrefs.SetFloat("Year", 1939);
+	}else if (Year.value == 79) {
+		PlayerPrefs.SetFloat("Year", 1938);
+	}else if (Year.value == 80) {
+		PlayerPrefs.SetFloat("Year", 1937);
+	}else if (Year.value == 81) {
+		PlayerPrefs.SetFloat("Year", 1936);
+	}else if (Year.value == 82) {
+		PlayerPrefs.SetFloat("Year", 1935);
+	}else if (Year.value == 83) {
+		PlayerPrefs.SetFloat("Year", 1934);
+	}else if (Year.value == 84) {
+		PlayerPrefs.SetFloat("Year", 1933);
+	}else if (Year.value == 85) {
+		PlayerPrefs.SetFloat("Year", 1932);
+	}else if (Year.value == 86) {
+		PlayerPrefs.SetFloat("Year", 1931);
+	}else if (Year.value == 87) {
+		PlayerPrefs.SetFloat("Year", 1930);
+	}else if (Year.value == 88) {
+		PlayerPrefs.SetFloat("Year", 1929);
+	}else if (Year.value == 89) {
+		PlayerPrefs.SetFloat("Year", 1928);
+	}else if (Year.value == 90) {
+		PlayerPrefs.SetFloat("Year", 1927);
+	}else if (Year.value == 91) {
+		PlayerPrefs.SetFloat("Year", 1926);
+	}else if (Year.value == 92) {
+		PlayerPrefs.SetFloat("Year", 1925);
+	}else if (Year.value == 93) {
+		PlayerPrefs.SetFloat("Year", 1924);
+	}else if (Year.value == 94) {
+		PlayerPrefs.SetFloat("Year", 1923);
+	}else if (Year.value == 95) {
+		PlayerPrefs.SetFloat("Year", 1922);
+	}else if (Year.value == 96) {
+		PlayerPrefs.SetFloat("Year", 1921);
+	}else if (Year.value == 97) {
+		PlayerPrefs.SetFloat("Year", 1920);
+	}else if (Year.value == 98) {
+		PlayerPrefs.SetFloat("Year", 19191);
+	}else if (Year.value == 99) {
+		PlayerPrefs.SetFloat("Year", 1918);
+	}else if (Year.value == 100) {
+		PlayerPrefs.SetFloat("Year", 1917);
+	}else if (Year.value == 101) {
+		PlayerPrefs.SetFloat("Year", 1916);
+	}else if (Year.value == 102) {
+		PlayerPrefs.SetFloat("Year", 1915);
+	}else if (Year.value == 103) {
+		PlayerPrefs.SetFloat("Year", 1914);
+	}else if (Year.value == 104) {
+		PlayerPrefs.SetFloat("Year", 1913);
+	}else if (Year.value == 105) {
+		PlayerPrefs.SetFloat("Year", 1912);
+	}else if (Year.value == 106) {
+		PlayerPrefs.SetFloat("Year", 1911);
+	}else if (Year.value == 107) {
+		PlayerPrefs.SetFloat("Year", 1910);
+	}else if (Year.value == 108) {
+		PlayerPrefs.SetFloat("Year", 1909);
+	}else if (Year.value == 109) {
+		PlayerPrefs.SetFloat("Year", 1908);
+	}else if (Year.value == 110) {
+		PlayerPrefs.SetFloat("Year", 1907);
+	}else if (Year.value == 111) {
+		PlayerPrefs.SetFloat("Year", 1906);
+	}else if (Year.value == 112) {
+		PlayerPrefs.SetFloat("Year", 1905);
+	}else if (Year.value == 113) {
+		PlayerPrefs.SetFloat("Year", 1904);
+	}else if (Year.value == 114) {
+		PlayerPrefs.SetFloat("Year", 1903);
+	}else if (Year.value == 115) {
+		PlayerPrefs.SetFloat("Year", 1902);
+	}else if (Year.value == 116) {
+		PlayerPrefs.SetFloat("Year", 1901);
+	}else if (Year.value == 117) {
+		PlayerPrefs.SetFloat("Year", 1900);
+	}
+}
